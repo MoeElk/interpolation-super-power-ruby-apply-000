854686d8918c189f75964d157db1175da7178b01
Mohamed Elkhaloui <elkhaloui@gmail.com>
2020-11-15 13:28:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,9 @@
-R= red
-G= green
-B= blue
-Y= yellow
-I= indigo
-V= violet
+R= "red"
+G= "green"
+B= "blue"
+Y= "yellow"
+I= "indigo"
+V= "violet"
 
 def display_rainbow
   puts "rainbow colors R:red, O:orange, G:green, B:blue, Y:yellow, I:indigo, V:violet"
